89621be05804f0e933bb1b42722a9f2eb076cf79
Eduardo Pineda <edibozu@gmail.com>
2020-09-12 19:27:46 -0400

    added the current date to display on the page, and created an html and js file to write example code

---
+++ localstorage.js
@@ -0,0 +1,27 @@
+localStorage.setItem("name", "Eduardo");
+localStorage.setItem("age", "30");
+localStorage.setItem("Occupation", "Student");
+console.log(localStorage);
+
+var occupationEl = localStorage.getItem("Occupation");
+console.log(occupationEl);
+
+localStorage.removeItem("user");
+console.log(localStorage);
+
+localStorage.key(3);
+console.log(localStorage.key(3));
+
+// 1. create html with rows, columns, time slots, text area, and save button.
+
+// 2. assign variables to classes and id's in html code.
+
+// 3. get current date and time from moments and assign it to a variable.
+
+// 4. format how i want date and time to appear on the screen.
+
+// 5. compare current time with past or future time to make sure that the text area shows correct color.
+
+// 6. create onClick functions for save buttons that will update local storage.
+
+
--- script.js
+++ script.js
@@ -5,8 +5,14 @@ $(document).ready(function() {
 // Set variables
 
 var containerEl = $(".container");
-var currentDayEl = $("#current-day");
+var currentDayEl = $("#currentDay");
 let m = moment();
+
+var todaysDate = (moment().format('MMMM Do YYYY, h:mm:ss a'));
+console.log(todaysDate);
+currentDayEl.text(todaysDate);
+
+
 
 
 
