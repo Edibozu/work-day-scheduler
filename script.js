baa5fed840492c2fa1d2b6118f4ad93845cf589e
Eduardo Pineda <edibozu@gmail.com>
2020-09-12 16:22:34 -0400

    assigning variables to classes and id's that will be used later

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 // Pre-step to make sure files are linked and js file is working
 console.log("Hello World!");
+$(document).ready(function() {
 
+// Set variables
+
+var containerEl = $(".container");
+var currentDayEl = $("#current-day");
+let m = moment();
+
+
+
+});
