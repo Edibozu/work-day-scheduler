3eb053d7cb99a6b9f949146d426b70520602f461
Eduardo Pineda <edibozu@gmail.com>
2020-09-12 20:40:00 -0400

    made a variable for the workhour times and made a for loop for it to grab the times from. also began writing code to dynamically create rows.

--- script.js
+++ script.js
@@ -8,9 +8,24 @@ var containerEl = $(".container");
 var currentDayEl = $("#currentDay");
 let m = moment();
 
-var todaysDate = (moment().format('MMMM Do YYYY, h:mm:ss a'));
+var todaysDate = (moment().format('MMMM Do YYYY, h:mm a'));
 console.log(todaysDate);
 currentDayEl.text(todaysDate);
+
+var workHours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
+
+for (i = 0; i < workHours.length; i++) {
+    console.log(workHours[i]);
+
+    var rowEl = $("<div class='row'>");
+
+    var hoursEl = $("<div class='col-sm-1 hour'>").html(workHours[i]);
+
+    
+
+
+
+}
 
 
 
