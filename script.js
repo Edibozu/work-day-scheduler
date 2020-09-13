d19bf0a7b75f27a23fc72eeaf9efd81bdef3c488
Eduardo Pineda <edibozu@gmail.com>
2020-09-12 21:03:47 -0400

    created the text area for the comments to go into and made sure to add the meta data for the blanks work with the correct hour

--- script.js
+++ script.js
@@ -17,11 +17,24 @@ var workHours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
 for (i = 0; i < workHours.length; i++) {
     console.log(workHours[i]);
 
+    // dynamically creating a new row for the hours to display
     var rowEl = $("<div class='row'>");
 
-    var hoursEl = $("<div class='col-sm-1 hour'>").html(workHours[i]);
+    var hoursEl = $("<div class='col-sm-1 hour'>").text(workHours[i]);
 
-    
+    rowEl.append(hoursEl);
+
+    containerEl.append(rowEl);
+
+    // dynamically creating the text area to write our comments in
+    var textAreaEl = $("<textarea class='description col-sm-10 time-block'>");
+    textAreaEl.attr("data-hour", workHours[i]);
+    rowEl.append(textAreaEl);
+    containerEl.append(rowEl);
+
+
+
+
 
 
 
